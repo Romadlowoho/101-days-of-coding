181660a0d36c53b61f26fdae9e80cbf21225357b
romaldowoho <roma.ushpayev@gmail.com>
2018-02-28 23:26:47 -0800

    implementation,math,Codeforces,easy

---
+++ dreamoon_and_stairs.cpp
@@ -0,0 +1,26 @@
+//
+#include <iostream>
+using namespace std;
+
+#define fr(a, n) for (int i = a; i < n; ++i)
+
+int main()
+{
+	int n,m;
+	cin >> n >> m;
+	int minCoins = n%2 + n/2;
+	int maxCoins = n;
+	int range = maxCoins - minCoins;
+	if (n < m) {cout << -1; return 0;}
+	if (n == m) {cout << n; return 0;}
+	fr(0,range)
+	{
+		if (minCoins%m == 0)
+		{
+			cout << minCoins; return 0;
+		} else
+		{
+			++minCoins;
+		}
+	}
+}
